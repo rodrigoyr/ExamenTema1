7c6eed00bbc4094f4aa126e8a6b66cc65b9feb02
rodrigoyr <ryeperub@myuax.com>
2023-11-09 12:34:16 +0100

    changing code because of error commit

--- Ejercicio 10/main.cpp
+++ Ejercicio 10/main.cpp
@@ -12,7 +12,7 @@ public:
     Profesor(const std::string& nombre, int edad, const std::string& materiaImparte, int anosDocencia)
             : nombre(nombre), edad(edad), materiaImparte(materiaImparte), anosDocencia(anosDocencia) {}
 
-    void mostrar_info_profesor() {
+    void mostrar_info_profesor() const {
         std::cout << "Información del profesor:" << std::endl;
         std::cout << "Nombre: " << nombre << std::endl;
         std::cout << "Edad: " << edad << " años" << std::endl;
