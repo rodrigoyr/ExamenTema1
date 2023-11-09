58a0cc30c43146607de048a66c5675bf4fc3efe9
rodrigoyr <ryeperub@myuax.com>
2023-11-09 12:12:34 +0100

    cambiar code commit

--- Ejercicio 6/main.cpp
+++ Ejercicio 6/main.cpp
@@ -1,51 +1,31 @@
 #include <iostream>
-#include <string>
 #include <vector>
 
-class RegistroAsistencia {
-public:
-    RegistroAsistencia(const std::string& fecha, const std::string& estado) : fecha(fecha), estado(estado) {}
-
-    void mostrar_asistencia() const {
-        std::cout << "Fecha de la clase: " << fecha << std::endl;
-        std::cout << "Estado de asistencia: " << estado << std::endl;
+double calcularPromedio(const std::vector<int>& numeros) {
+    int suma = 0;
+    for(int numero : numeros) {
+        suma += numero;
     }
-
-private:
-    std::string fecha;
-    std::string estado;
-};
+    return static_cast<double>(suma) / numeros.size();
+}
 
 int main() {
-    std::vector<RegistroAsistencia> asistencias;
+    std::vector<int> numeros;
+    int n;
 
-    while (true) {
-        std::string fecha, estado;
-        std::cout << "Ingrese la fecha de la clase (o escriba 'fin' para terminar): ";
-        std::cin.ignore();
-        std::getline(std::cin, fecha);
+    std::cout << "Ingrese la cantidad de números: ";
+    std::cin >> n;
 
-        if (fecha == "fin") {
-            break;
-        }
-
-        std::cout << "Estado de asistencia (asistio, falta, tardanza): ";
-        std::cin >> estado;
-
-        if (estado != "asistio" && estado != "falta" && estado != "tardanza") {
-            std::cout << "Estado de asistencia no válido. Debe ser 'asistio', 'falta' o 'tardanza'." << std::endl;
-            continue;
-        }
-
-        RegistroAsistencia registro(fecha, estado);
-        asistencias.push_back(registro);
+    for(int i = 0; i < n; i++) {
+        int numero;
+        std::cout << "Ingrese el número " << (i + 1) << ": ";
+        std::cin >> numero;
+        numeros.push_back(numero);
     }
 
-    std::cout << "Asistencias registradas:" << std::endl;
-    for (const RegistroAsistencia& registro : asistencias) {
-        registro.mostrar_asistencia();
-        std::cout << std::endl;
-    }
+    double promedio = calcularPromedio(numeros);
+
+    std::cout << "El promedio es: " << promedio << std::endl;
 
     return 0;
 }
