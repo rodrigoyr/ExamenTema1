51d16fd82a5a01af785660d332c9e4ad1b2e0afc
rodrigoyr <ryeperub@myuax.com>
2023-11-09 12:24:54 +0100

    trying another code commit

--- Ejercicio 8/main.cpp
+++ Ejercicio 8/main.cpp
@@ -1,50 +1,67 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 
 class Estudiante {
 public:
     std::string nombre;
     int edad;
-    std::string grado;
 
-    Estudiante(const std::string& nombre, int edad, const std::string& grado)
-            : nombre(nombre), edad(edad), grado(grado) {}
+    Estudiante(const std::string& nombre, int edad) : nombre(nombre), edad(edad) {}
 };
 
-std::vector<Estudiante> filtrarEstudiantesPorGrado(const std::vector<Estudiante>& estudiantes, const std::string& grado) {
-    std::vector<Estudiante> estudiantesFiltrados;
-    for (const Estudiante& estudiante : estudiantes) {
-        if (estudiante.grado == grado) {
-            estudiantesFiltrados.push_back(estudiante);
-        }
-    }
-    return estudiantesFiltrados;
-}
-
 int main() {
-    // Crear una lista de estudiantes
-    std::vector<Estudiante> listaEstudiantes;
-    listaEstudiantes.push_back(Estudiante("Juan", 20, "A"));
-    listaEstudiantes.push_back(Estudiante("María", 19, "B"));
-    listaEstudiantes.push_back(Estudiante("Pedro", 21, "A"));
-    listaEstudiantes.push_back(Estudiante("Ana", 18, "C"));
+    std::map<std::string, std::vector<Estudiante>> grados;  // Mapa para almacenar estudiantes por grado
 
-    // Pedir al usuario el grado a filtrar
-    std::string gradoFiltrar;
-    std::cout << "Ingrese el grado a filtrar: ";
-    std::cin >> gradoFiltrar;
+    while (true) {
+        int opcion;
+        std::cout << "Opciones:" << std::endl;
+        std::cout << "1. Agregar estudiante a un grado" << std::endl;
+        std::cout << "2. Mostrar estudiantes por grado" << std::endl;
+        std::cout << "3. Salir" << std::endl;
+        std::cout << "Elija una opción: ";
+        std::cin >> opcion;
 
-    // Filtrar estudiantes por grado
-    std::vector<Estudiante> estudiantesFiltrados = filtrarEstudiantesPorGrado(listaEstudiantes, gradoFiltrar);
+        if (opcion == 1) {
+            std::string grado;
+            std::cout << "Ingrese el nombre del estudiante: ";
+            std::string nombre;
+            std::cin.ignore();
+            std::getline(std::cin, nombre);
+            std::cout << "Ingrese la edad del estudiante: ";
+            int edad;
+            std::cin >> edad;
 
-    // Mostrar los estudiantes del grado especificado
-    if (estudiantesFiltrados.empty()) {
-        std::cout << "No se encontraron estudiantes en el grado " << gradoFiltrar << std::endl;
-    } else {
-        std::cout << "Estudiantes en el grado " << gradoFiltrar << ":" << std::endl;
-        for (const Estudiante& estudiante : estudiantesFiltrados) {
-            std::cout << "Nombre: " << estudiante.nombre << ", Edad: " << estudiante.edad << std::endl;
+            std::cout << "Ingrese el grado al que pertenece el estudiante: ";
+            std::cin.ignore();
+            std::getline(std::cin, grado);
+
+            Estudiante estudiante(nombre, edad);
+
+            // Agregar el estudiante al grado correspondiente
+            grados[grado].push_back(estudiante);
+
+            std::cout << "Estudiante agregado al grado " << grado << std::endl;
+        } else if (opcion == 2) {
+            std::string grado;
+            std::cout << "Ingrese el grado para ver los estudiantes: ";
+            std::cin.ignore();
+            std::getline(std::cin, grado);
+
+            // Verificar si el grado existe en el mapa
+            if (grados.find(grado) != grados.end()) {
+                std::cout << "Estudiantes en el grado " << grado << ":" << std::endl;
+                for (const Estudiante& estudiante : grados[grado]) {
+                    std::cout << "Nombre: " << estudiante.nombre << ", Edad: " << estudiante.edad << std::endl;
+                }
+            } else {
+                std::cout << "El grado especificado no existe." << std::endl;
+            }
+        } else if (opcion == 3) {
+            break;
+        } else {
+            std::cout << "Opción no válida. Por favor, elija una opción válida." << std::endl;
         }
     }
 
