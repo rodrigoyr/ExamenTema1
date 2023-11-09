813b964e20db517297d8220463200340c8213b56
rodrigoyr <ryeperub@myuax.com>
2023-11-09 11:48:26 +0100

    first commit

--- Ejercicio 2/main.cpp
+++ Ejercicio 2/main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <string>
+
+class Estudiante {
+public:
+    std::string nombre;
+    int edad;
+    std::string grado;
+
+    void mostrar_info() {
+        std::cout << "Nombre: " << nombre << std::endl;
+        std::cout << "Edad: " << edad << " años" << std::endl;
+        std::cout << "Grado: " << grado << std::endl;
+    }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Estudiante estudiante;
+
+    // Ingresar información del estudiante
+    std::cout << "Ingrese el nombre del estudiante: ";
+    std::getline(std::cin, estudiante.nombre);
+
+    std::cout << "Ingrese la edad del estudiante: ";
+    std::cin >> estudiante.edad;
+    std::cin.ignore(); // Limpiar el buffer del teclado
+
+    std::cout << "Ingrese el grado del estudiante: ";
+    std::getline(std::cin, estudiante.grado);
+
+    // Mostrar la información del estudiante
+    std::cout << "\nInformación del estudiante:" << std::endl;
+    estudiante.mostrar_info();
+
     return 0;
 }
