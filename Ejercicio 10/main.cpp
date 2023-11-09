aee157c21cd09941c8de1e1ffb3af246cf7281b7
rodrigoyr <ryeperub@myuax.com>
2023-11-09 12:35:03 +0100

    changing code for better results commit

--- Ejercicio 10/main.cpp
+++ Ejercicio 10/main.cpp
@@ -25,35 +25,45 @@ int main() {
     std::vector<Profesor> listaProfesores;
 
     while (true) {
-        std::string nombre, materiaImparte;
-        int edad, anosDocencia;
+        int opcion;
+        std::cout << "Opciones:" << std::endl;
+        std::cout << "1. Ver profesores registrados" << std::endl;
+        std::cout << "2. Agregar nuevo profesor" << std::endl;
+        std::cout << "3. Salir" << std::endl;
+        std::cout << "Elija una opción: ";
+        std::cin >> opcion;
 
-        std::cout << "Ingrese el nombre del profesor (o escriba 'fin' para terminar): ";
-        std::cin.ignore();
-        std::getline(std::cin, nombre);
+        if (opcion == 1) {
+            std::cout << "Listado de profesores:" << std::endl;
+            for (const Profesor& profesor : listaProfesores) {
+                profesor.mostrar_info_profesor();
+                std::cout << std::endl;
+            }
+        } else if (opcion == 2) {
+            std::string nombre, materiaImparte;
+            int edad, anosDocencia;
 
-        if (nombre == "fin") {
+            std::cout << "Ingrese el nombre del profesor: ";
+            std::cin.ignore();
+            std::getline(std::cin, nombre);
+
+            std::cout << "Ingrese la edad del profesor: ";
+            std::cin >> edad;
+            std::cout << "Ingrese la materia que imparte el profesor: ";
+            std::cin.ignore();
+            std::getline(std::cin, materiaImparte);
+            std::cout << "Ingrese los años de docencia del profesor: ";
+            std::cin >> anosDocencia;
+
+            Profesor nuevoProfesor(nombre, edad, materiaImparte, anosDocencia);
+            listaProfesores.push_back(nuevoProfesor);
+
+            std::cout << "Profesor registrado." << std::endl;
+        } else if (opcion == 3) {
             break;
+        } else {
+            std::cout << "Opción no válida. Por favor, elija una opción válida." << std::endl;
         }
-
-        std::cout << "Ingrese la edad del profesor: ";
-        std::cin >> edad;
-        std::cout << "Ingrese la materia que imparte el profesor: ";
-        std::cin.ignore();
-        std::getline(std::cin, materiaImparte);
-        std::cout << "Ingrese los años de docencia del profesor: ";
-        std::cin >> anosDocencia;
-
-        Profesor nuevoProfesor(nombre, edad, materiaImparte, anosDocencia);
-        listaProfesores.push_back(nuevoProfesor);
-
-        std::cout << "Profesor registrado." << std::endl;
-    }
-
-    std::cout << "Listado de profesores:" << std::endl;
-    for (const Profesor& profesor : listaProfesores) {
-        profesor.mostrar_info_profesor();
-        std::cout << std::endl;
     }
 
     return 0;
