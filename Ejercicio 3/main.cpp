a87c65f44d5e9deefc177f87f0a9c7788918d4b0
rodrigoyr <ryeperub@myuax.com>
2023-11-09 11:52:58 +0100

    first code commit

---
+++ Ejercicio 3/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+void intercambiar(int *ptr1, int *ptr2) {
+    int temp = *ptr1;
+    *ptr1 = *ptr2;
+    *ptr2 = temp;
+}
+
+int main() {
+    int num1, num2;
+
+    std::cout << "Ingrese el primer número: ";
+    std::cin >> num1;
+
+    std::cout << "Ingrese el segundo número: ";
+    std::cin >> num2;
+
+    std::cout << "Valores originales: num1 = " << num1 << ", num2 = " << num2 << std::endl;
+
+    // Llamamos a la función intercambiar utilizando punteros
+    intercambiar(&num1, &num2);
+
+    std::cout << "Valores intercambiados: num1 = " << num1 << ", num2 = " << num2 << std::endl;
+
+    return 0;
+}
